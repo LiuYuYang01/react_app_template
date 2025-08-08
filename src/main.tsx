02e6a5587b6e6f937a8352db09e4076d37d3572f
宇阳 <liuyuyang1024@yeah.net>
2025-08-08 14:05:40 +0800

    feat: 添加react-router支持并重构页面路由结构
    将App组件移至pages/home目录下
    添加react-router依赖并配置基本路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from '@/pages/home';
 import './styles/global.css';
 
 const app = createRoot(document.getElementById('root')!);
-app.render(<App />);
+app.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
