135596d6cd12989818e46af6df4d765a4d7b058e
宇阳 <liuyuyang1024@yeah.net>
2026-01-06 11:24:16 +0800

    refactor: 移除不再使用的按钮组件
    删除了 Apple、Google 和 PayPal 按钮组件，更新首页以简化结构并提升可读性。

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,7 @@
-import Paypal from '@/components/Button/Paypal';
-import Google from '@/components/Button/Google';
-import Apple from '@/components/Button/Apple';
-
 export default () => {
   return (
     <>
-      <div className="space-y-2 space-x-2">
-        <Paypal />
-        <Google />
-        <Apple />
-      </div>
+      <div className="text-9xl text-white text-center p-10 bg-blue-500">Hello</div>
     </>
   );
 };
